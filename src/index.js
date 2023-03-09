0b05e71f63dbe4db483149c5f947e7dbdf8e2156
L-E-Crane <107731061+L-E-Crane@users.noreply.github.com>
2023-03-09 13:03:18 -0800

    changed to SheCodes API

--- src/index.js
+++ src/index.js
@@ -26,31 +26,64 @@ function formatDateTime() {
 let dateTimeDisplay = document.querySelector("#date-time");
 dateTimeDisplay.innerHTML = formatDateTime();
 
-function showTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let description = response.data.weather[0].description;
+function showTemperature(response) {
+  console.log(response);
+  let temperature = Math.round(response.data.temperature.current);
+  let description = response.data.condition.description;
+  let searchedCity = response.data.city;
   let windspeed = response.data.wind.speed;
-  let searchedCity = response.data.name;
-  let cityHeading = document.querySelector("#city-header");
-  cityHeading.innerHTML = searchedCity;
-  let cityTemp = document.querySelector("#big-temp");
-  cityTemp.innerHTML = `${temp}°C`;
-  let weatherDescription = document.querySelector("#big-descrip");
+  let currentIcon = document.querySelector("#current-icon");
+  let currentCity = document.querySelector("#current-city");
+  let currentTemperature = document.querySelector("#current-temperature");
+  let weatherDescription = document.querySelector("#current-description");
+  let currentWindspeed = document.querySelector("#current-windspeed");
+
+  celsiusTemperature = Math.round(response.data.temperature.current);
+
+  currentCity.innerHTML = searchedCity;
+  currentTemperature.innerHTML = `${temperature}`;
   weatherDescription.innerHTML = description;
-  let thewindspeed = document.querySelector("#windspeed");
-  thewindspeed.innerHTML = `windspeed: ${windspeed} mph`;
+  currentWindspeed.innerHTML = `Wind: ${windspeed} km/h`;
+  currentIcon.setAttribute(
+    "src",
+    `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
+  );
+  currentIcon.setAttribute("alt", response.data.condition.description);
 }
 
 function citySearch(city) {
-  let apiKey = `bb0df6985c2eab6a171d64a6bacbb4e1`;
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showTemp);
+  let apiKey = `a32f490009bo3303b804aa52038tee62`;
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(url).then(showTemperature);
 }
 
 function submitSearch(event) {
   event.preventDefault();
-  let cityInput = document.querySelector("#search-city");
+  let cityInput = document.querySelector("#city-search");
   citySearch(cityInput.value);
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", submitSearch);
+
+////////
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#current-temperature");
+  temperatureElement.innerHTML = celsiusTemperature;
+}
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+citySearch("Portland");
